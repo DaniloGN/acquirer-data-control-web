68511968c442285daee0d40ea705a201d7b2b7a4
DaniloGN <dan_guarizzo@hotmail.com>
2019-07-01 23:17:21 -0300

    + criar novo contrato pagina contratos

--- vue/src/services/ContractService.js
+++ vue/src/services/ContractService.js
@@ -5,7 +5,15 @@ export default {
         return Api().get('contratos')
     },
     postContracts (client, acquirer, establishment) {
-        return Api().post('contratos', {body: {client: client, acquirer: acquirer, establishment: establishment}}, {headers: {"Content-Type": "application/json"}})
+        delete client["contract_number"]
+        delete acquirer["contract_number"]
+        Api().post('contratos',{client: client, acquirer: acquirer, establishment: establishment}, {headers: {"Content-Type": "application/json"}})
+            .then((response)=>{
+                return response
+            })
+            .catch((err)=>{
+                return err
+            })
     },
     async deleteContract(id) {
         return await Api().delete('contratos',{ data: id });
--- vue/src/views/Contracts.vue
+++ vue/src/views/Contracts.vue
@@ -14,18 +14,42 @@
                     </tr>
                 </template>
             </v-data-table>
+            <v-fab-transition>
+                <v-btn @click="openContractDialog()" color="cyan darken-3" dark fab fixed bottom left><v-icon>add</v-icon></v-btn>
+            </v-fab-transition>
+            <v-dialog v-model="contractDialog" max-width="500px">
+                <v-card>
+                    <v-card-title>
+                        <h3>Adicionar um contrato</h3>
+                    </v-card-title>
+                    <v-card-text>
+                        <v-select return-object outline v-model="selectedClient" :items="clients" label="Selecione o cliente" item-value="id" item-text="name"></v-select>
+                        <v-select return-object outline v-model="selectedAcquirer" :items="acquirers" label="Selecione o adquirente" item-value="id" item-text="name"></v-select>
+                        <v-text-field v-model="establishment" label="Número do estabelecimento" outline counter="10" type="number"></v-text-field>
+                    </v-card-text>
+                    <v-card-actions>
+                        <v-spacer></v-spacer>
+                        <v-btn color="primary" flat @click="contractDialog=false">Close</v-btn>
+                        <v-btn color="primary" :disabled="confirmSelected()" :loading="loading" flat @click="addContract()">Adicionar</v-btn>
+                    </v-card-actions>
+                </v-card>
+            </v-dialog>
         </v-flex>
     </v-layout>
 </template>
 
 <script>
     import ContractService from '../services/ContractService.js'
+    import ClientService from '../services/ClientService.js'
+    import AcquirerService from '../services/AcquirerService.js'
+
     export default {
         components: {
         },
         data () {
             return {
                 loading: false,
+                contractDialog: false,
                 headers: [
                     { text: 'Cliente', value: 'client', sortable: true},
                     { text: 'Adquirente', value: 'acquirer', sortable: true},
@@ -34,14 +58,17 @@
                     {value: 'actions'}
                 ],
                 items: [],
+                clients: [],
+                acquirers: [],
+                selectedClient: null,
+                selectedAcquirer: null,
+                establishment: null,
             }
         },
         mounted(){
             this.loading = true;
             ContractService.getContracts().then((response)=>{
                 this.items = response.data
-                // eslint-disable-next-line no-console
-                console.log(response.data)
                 this.loading = false;
             })
         },
@@ -56,6 +83,21 @@
                     let response = await ContractService.deleteContract(contract.id)
                     if(response.status == 200) this.items.splice(this.items.indexOf(contract), 1)
                 }
+            },
+            async openContractDialog(){
+                if(this.clients.length == 0) await ClientService.getClients().then((response)=>{ this.clients = response.data})
+                if(this.acquirers.length == 0) await AcquirerService.getAcquirers().then((response)=>{ this.acquirers = response.data})
+                this.contractDialog = true
+            },
+            confirmSelected(){
+                if(this.selectedClient != null && this.selectedAcquirer != null && this.establishment != null) return false
+                else return true
+            },
+            async addContract(){
+                this.loading = true
+                let response = await ContractService.postContracts(this.selectedClient, this.selectedAcquirer, parseInt(this.establishment))
+                this.loading = false
+                this.contractDialog = false
             }
         },
     }
@@ -63,5 +105,4 @@
 </script>
 
 <style lang="sass">
-
 </style>
